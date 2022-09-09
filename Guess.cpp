51013aa6f6ea202dacc7061de2eda102280fa0c1
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-09-09 12:00:28 -0700

    Another commit.

--- Guess.cpp
+++ Guess.cpp
@@ -1,1 +1,17 @@
-this is guess test 
+#include <cstddef>
+#include <cstdio>
+#include <time.h>
+#include <stdlib.h>
+
+
+
+
+int main () {
+
+
+  srand (time(NULL));
+
+  printf("123123123123123123");
+
+  
+}
