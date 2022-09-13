8b76af9b576eb4e757faa71eaaf47e8fd992f0f9
U-BSD\462750 <462750@SHS-DGCVCS2.beaverton.k12.or.us>
2022-09-13 12:23:27 -0700

    Almost ready

--- Guess.cpp
+++ Guess.cpp
@@ -1,20 +1,44 @@
-#include <cstddef>
-#include <cstdio>
-#include <time.h>
+#include <iostream>
 #include <stdlib.h>
+#include <time.h>
 
-
-int Number;
+using namespace std;
 
 int main () {
 
-  int Number;
+  int Number, UserInput, Tries;
+  char PlayAgain, Playing;
 
+  PlayAgain = 'y';
+  Tries = 1;
+  
+  do {
+  
   srand (time(NULL));
 
   Number = rand() % 100 + 1;
-  
-  printf (Number);
 
-  
+  do {
+  cout << Number << endl;
+  cin >> UserInput;
+  if (UserInput > Number) {
+    cout << "Your guess is higher than the secret number" << endl;
+  Tries = Tries + 1;
+  }
+  else if (Number>UserInput) {
+    cout << "Your guess is lower than the secret number" << endl;
+  Tries = Tries + 1;
+  }
+  } while (Number != UserInput);
+  cout << "You guessed the correct number! It took you" << Tries << endl;
+  cout << "Play again?" << endl;
+  cin << PlayAgain;
+
+  if (PlayAgain == 'y') {
+  cout << "Playing Again." << endl;
+  }
+  else if (PlayAgain != 'y') {
+  PlayAgain = 'n';
+  }
+  } while (Playing == 'y');
 }
